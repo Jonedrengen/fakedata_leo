0eca4659eeceea329acdb08f383a5f4d1446f938
Jon Slotved <127400053+Jonedrengen@users.noreply.github.com>
2024-08-26 15:24:58 +0200

    chore: Add Batch and Sample data generation and CSV writing functionality

--- Batch.py
+++ Batch.py
@@ -2,6 +2,7 @@ from faker import Faker
 from faker.providers import BaseProvider #custom providers
 import random
 import csv
+from datetime import datetime
 
 fake = Faker()
 
@@ -9,18 +10,28 @@ fake = Faker()
 Batch_headers = ["BatchID", "BatchDate", "Platform", "BatchSource", "TimestampCreated", "TimestampUpdated"]
 
 
-
 def BatchData(record_amount):
     Batch_data = []
     for i in range(record_amount):
         record = {
-            "BatchID": fake.uuid4(),
+            "BatchID": fake.random_element(elements=("someID","someotherID")) + str(random.randint(0, 999999999)).zfill(9),
             "BatchDate": fake.date(),
             "Platform": fake.random_element(elements=('labA','labB','labC')),
-            "BatchSource": fake.company(),
-            "TimestampCreated": fake.date_time_this_year(),
-            "TimestampCreated": fake.date_time_this_year(),
-            
+            "BatchSource": fake.random_element(elements=('SourceA','SourceB','SourceC')),
+            "TimestampCreated": str(datetime.now()),
+            "TimestampUpdated": str(datetime.now())
         }
+        Batch_data.append(record)
+    print(f"Batch data generated {record_amount} times")
+    return Batch_data
 
-    
+def write_to_csv(file_name, data, headers):
+    with open(file_name, mode='w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=headers)
+        writer.writeheader()
+        writer.writerows(data)
+    print(f"written to {file_name}")
+
+batch_data = BatchData(1000)
+
+write_to_csv('batch_data.csv', batch_data, Batch_headers)
---
+++ Sample.py
@@ -0,0 +1,39 @@
+from faker import Faker
+from faker.providers import BaseProvider #custom providers
+import random
+import csv
+from datetime import datetime
+
+fake = Faker()
+
+#BatchID is primary key
+Sample_headers = ["SampleID", "SampleDateTime", "Host", "Ct", "DateSampling", "CurrentConsensusID", "TimestampCreated", "TimestampUpdated"]
+
+
+def SampleData(record_amount):
+    Sample_data = []
+    for i in range(record_amount):
+        record = {
+            "SampleID": fake.random_element(elements=("someID","someotherID")) + str(random.randint(0, 999999999)).zfill(9),
+            "SampleDateTime": fake.date(),
+            "Host": fake.random_element(elements=("HostA", "HostB", "HostC")),
+            "Ct": fake.random_element(elements=("CtA", "CtB", "CtC")),
+            "DateSampling": fake.random_element(elements=("DateSamplingX", "DateSamplingY", "DateSamplingZ")),
+            "CurrentConsensusID": fake.random_element(elements=("anID", "anotherID", "thirdID")),
+            "TimestampCreated": str(datetime.now()),
+            "TimestampUpdated": str(datetime.now())
+        }
+        Sample_data.append(record)
+    print(f"Sample data generated {record_amount} times")
+    return Sample_data
+
+def write_to_csv(file_name, data, headers):
+    with open(file_name, mode='w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=headers)
+        writer.writeheader()
+        writer.writerows(data)
+    print(f"written to {file_name}")
+
+sample_data = SampleData(1000)
+
+write_to_csv('Sample_data.csv', sample_data, Sample_headers)
